d2143f8a0d67da24c5145c748285adb5571ac67d
mcanmor0312 <151721613+mcanmor0312@users.noreply.github.com>
2024-10-24 11:31:16 +0200

    codigo

--- tema-04/ejemplos/05/class/class.libros.php
+++ tema-04/ejemplos/05/class/class.libros.php
@@ -28,5 +28,53 @@
         
         
     }
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getTitulo() {
+        return $this->titulo;
+    }
+    
+    public function getPrecio() {
+        return $this->precio;
+    }
+    
+    public function getPaginas() {
+        return $this->paginas;
+    }
+    
+    public function getAutor() {
+        return $this->autor;
+    }
+    
+    public function getTematica() {
+        return $this->tematica;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+    
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+    
+    public function setPaginas($paginas) {
+        $this->paginas = $paginas;
+    }
+    
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+    
+    public function setTematica($tematica) {
+        $this->tematica = $tematica;
+    }
+    
+    
 
  }
--- tema-04/ejemplos/05/index.php
+++ tema-04/ejemplos/05/index.php
@@ -1,12 +1,28 @@
 <?php
-#incluyo archivo de la clase vehiculo
-include("class/class.vehiculo.php");
-#creo un objeto de la clase vehiculo
-$vehiculo=new Class_vehiculo();
-#Establecer la matricula del vehiculo
-$vehiculo->matricula='6712HRM';
-#Establecer la velocidad del vehiculo
-$vehiculo->setvelocidad(10);
-echo"Matrícula: ".$vehiculo->getmatricula();
-echo "<br>";
-echo"Velocidad: ".$vehiculo->getvelocidad();
+#incluyo archivo de la clase libro
+include("class/class.libros.php");
+#creo un objeto de la clase libro
+$libro1=new Class_libro();
+#mostrar detalles clase libro
+$libro1->setId(1);
+$libro1->setTitulo("Me cago en la leshe");
+$libro1->setPaginas(200);
+$libro1->setPrecio(12);
+$libro1->setAutor("Gandalf el gris");
+$libro1->setTematica(["Novela", "Drama", "Romance"]);
+
+
+
+
+echo'Id: '.$libro1->getId();
+echo'<br>';
+echo'Autor: '.$libro1->getAutor();
+echo'<br>';
+echo'Paginas: '.$libro1->getPaginas();
+echo'<br>';
+echo'Precio: '.$libro1->getPrecio();
+echo'<br>';
+echo'Tematica: '.implode(', ',$libro1->getTematica());
+echo'<br>';
+echo'Titulo: '.$libro1->getTitulo();
+echo'<br>';
